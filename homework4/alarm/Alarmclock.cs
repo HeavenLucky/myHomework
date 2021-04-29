919567de814b7142351bdcd2a0a82f90672079a7
HeavenLucky <64513891+HeavenLucky@users.noreply.github.com>
2021-04-30 01:52:18 +0800

    Add files via upload

---
+++ homework4/alarm/Alarmclock.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Threading;
+
+namespace alarm
+{
+
+    public class TickEventArgs : EventArgs {
+       public int hour = DateTime.Now.Hour;
+       public int mimute = DateTime.Now.Minute;
+    }
+    public class AlarmEventArgs : EventArgs
+    {
+       public int hour = DateTime.Now.Hour;
+       public int mimute = DateTime.Now.Minute;
+    }
+    public delegate void TickEventHandler(object sender, TickEventArgs tickEventArgs);
+
+    public delegate void AlarmEventHandler(object sender,AlarmEventArgs alarmEventArgs);
+    public class Alarmclock
+    {
+            private DateTime addatetime;
+
+            public event TickEventHandler Tick;
+
+        public event AlarmEventHandler Alarm;
+        public Alarmclock()
+        {
+
+        }
+       
+        public void Ticking() {
+
+            string advancedTime = Console.ReadLine();
+            while (true)
+            {
+                TickEventArgs tickEventArgs = new TickEventArgs();
+            AlarmEventArgs alarmEventArgs = new AlarmEventArgs();
+            DateTime addatetime = Convert.ToDateTime(advancedTime);
+
+           int  adminute = addatetime.Minute;
+            int adhour = addatetime.Hour;
+
+          
+                Tick(this, tickEventArgs);
+                Thread.Sleep(1000);
+
+                if (adhour == DateTime.Now.Hour && adminute == DateTime.Now.Minute)
+                {
+                    Alarm(this, alarmEventArgs);
+                }
+            }
+          
+        }
+
+       
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("输入预定时间");
+            var alarm = new Alarmclock();
+            alarm.Tick += ShowTime;
+            alarm.Alarm += Alarm;
+            while (true) { alarm.Ticking(); }
+        
+
+        }
+        public static void ShowTime(object sender, TickEventArgs tickEventArgs)
+        {
+            Console.WriteLine("现在的时间是：" + tickEventArgs.hour+"时"+tickEventArgs.mimute+"分");
+
+        }
+        public static void Alarm(object sender, AlarmEventArgs alarmEventArgs) {
+            Console.WriteLine("到点啦！！");
+        }
+        
+       
+
+
+
+    }
+}

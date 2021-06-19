16dc049f3926c2239936b2844f48b2241170f146
HeavenLucky <64513891+HeavenLucky@users.noreply.github.com>
2021-06-19 16:44:32 +0800

    homework10 difficult
    homework10 difficult

---
+++ homework10/Crawler/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+//1）只爬取某个指定网站上的网页 
+ //（2）只有当爬取的是html / html / aspx / jsp等网页时，才解析并爬取下一级URL。
+ //（3）相对地址(test / page.html, ./ test / page.html, .. / test2 / page2.html, / test3 / page.html)转成完整地址进行爬取。
+// （4） 尝试使用Winform来配置初始URL，启动爬虫，显示已经爬取的URL和错误的URL信息。
+
+namespace SimpleCrawler
+{
+    class SimpleCrawler
+    {
+       // private Hashtable urls = new Hashtable();
+       // private int count = 0;
+        private Dictionary<string, bool> done = new Dictionary<string, bool>();
+
+        //待下载队列
+        private Queue<string> pending = new Queue<string>();
+
+        //URL检测表达式，用于在HTML文本中查找URL
+        private readonly string urlDetectRegex = @"(href|HREF)[]*=[]*[""'](?<url>[^""'#>]+)[""']";
+
+        //URL解析表达式
+        public static readonly string urlParseRegex = @"^(?<site>https?://(?<host>[\w.-]+)(:\d+)?($|/))(\w+/)*(?<file>[^#?]*)";
+       
+        public string HostFilter { get; set; } //主机过滤规则
+        public string FileFilter { get; set; } //文件过滤规则
+        public int MaxPage { get; set; } //最大下载数量
+        public string StartURL { get; set; } //起始网址
+        public Encoding HtmlEncoding { get; set; } //网页编码
+        public Dictionary<string, bool> DownloadedPages { get; } //已下载网页
+        static void Main(string[] args)
+        {
+            SimpleCrawler myCrawler = new SimpleCrawler();
+            string startUrl = "http://www.cnblogs.com/dstang2000/";
+            if (args.Length >= 1) startUrl = args[0];
+            myCrawler.urls.Add(startUrl, false);//加入初始页面
+            new Thread(myCrawler.Crawl).Start();
+        }
+
+         public Crawler()
+        {
+            MaxPage = 100;
+            HtmlEncoding = Encoding.UTF8;
+        }
+
+        public void Excute(){
+           done.Clear();
+            pending.Clear();
+            pending.Enqueue(StartURL);
+            Crawl();
+  
+        }
+        private void Crawl()
+        {
+            //爬取
+            Console.WriteLine("开始爬行了.... ");
+            while (done.Count < MaxPage && pending.Count > 0)
+            {
+                string url = pending.Dequeue();
+                try
+                {
+                    string html = DownLoad(url); // 下载
+                    done[url] = true;
+                    PageDownloaded(this, url, "success");
+                    Parse(html, url);//解析,并加入新的链接
+                }
+                catch (Exception ex)
+                {
+                    PageDownloaded(this, url, "  Error:" + ex.Message);
+                }
+            }
+            CrawlerStopped(this);
+
+        }
+
+        public string DownLoad(string url)
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.Encoding = Encoding.UTF8;
+                string html = webClient.DownloadString(url);
+                string fileName = count.ToString();
+                File.WriteAllText(fileName, html, Encoding.UTF8);
+                return html;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return "";
+            }
+        }
+
+        private string FixUrl(string url, string pageUrl)
+        {
+            if (url.Contains("://"))
+            {
+                return url;
+            }
+            if (url.StartsWith("/"))
+            {
+                Match urlMatch = Regex.Match(pageUrl, urlParseRegex);
+                String site = urlMatch.Groups["site"].Value;
+                return site.EndsWith("/") ? site + url.Substring(1) : site + url;
+            }
+
+            if (url.StartsWith("../"))
+            {
+                url = url.Substring(3);
+                int idx = pageUrl.LastIndexOf('/');
+                return FixUrl(url, pageUrl.Substring(0, idx));
+            }
+
+            if (url.StartsWith("./"))
+            {
+                return FixUrl(url.Substring(2), pageUrl);
+            }
+            if (url.Contains("://"))
+            {
+                return url;
+            }
+            if (url.StartsWith("//"))
+            {
+                return "http:" + url;
+            }
+
+            int end = pageUrl.LastIndexOf("/");
+            return pageUrl.Substring(0, end) + "/" + url;
+        }
+
+        private void Parse(string html)
+        {
+            string strRef = @"(href|HREF)[]*=[]*[""'][^""'#>]+[""']";
+            MatchCollection matches = new Regex(strRef).Matches(html);
+            foreach (Match match in matches)
+            {
+                //strRef = match.Value.Substring(match.Value.IndexOf('=') + 1)
+              //            .Trim('"', '\"', '#', '>');
+               // if (strRef.Length == 0) continue;
+               // if (urls[strRef] == null) urls[strRef] = false;
+               string linkUrl =match.Groups["url"].Value;
+               if(linkUrl==null||linkUrl=="") continue;
+                linkUrl = FixUrl(linkUrl, startUrl);//转绝对路径
+                                                    //解析出host和file两个部分，进行过滤
+                Match linkUrlMatch = Regex.Match(linkUrl, urlParseRegex);
+                string host = linkUrlMatch.Groups["host"].Value;
+                string file = linkUrlMatch.Groups["file"].Value;
+                if (Regex.IsMatch(host, HostFilter) && Regex.IsMatch(file, FileFilter)
+                  && !done.ContainsKey(linkUrl))
+                {
+                    pending.Enqueue(linkUrl);
+                }
+
+            }
+        }
+    }
+}
